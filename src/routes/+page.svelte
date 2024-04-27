d04a095b1b405e8c5a9fded6e4dc32c99e50d2c7
sparshg <43041139+sparshg@users.noreply.github.com>
2024-04-28 03:41:38 +0530

    first steps

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,35 @@
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
-
-<style lang="postcss">
-	:global(html) {
-		background-color: theme(colors.blue.300);
-	}
-</style>
+<div class="flex mx-12 my-16">
+	<div class="w-3/5">
+		<h1 class="font-bold text-6xl mb-7">Sparsh Goenka</h1>
+		<p class="text-3xl font-mono">> echo hello World</p>
+	</div>
+	<div class="w-2/5">
+		<div class="card bg-base-200 p-0 rounded-2xl hover:scale-105 transition duration-300">
+			<figure>
+				<img
+					src="https://raw.githubusercontent.com/sparshg/asteroids-genetic/main/assets/demo.png"
+					alt="Asteroids"
+				/>
+			</figure>
+			<div class="card-body">
+				<h2 class="card-title">
+					Interactive AI Training Simulation
+					<div class="badge badge-secondary">NEW</div>
+				</h2>
+				<p>
+					Train your own AI to play asteroids game using neural networks and genetic algorithm that
+					uses the rules of natural selection to evolve. Save the best AI or load it later or play
+					the game yourself!
+				</p>
+				<div class="card-actions justify-between pt-2">
+					<div>
+						<div class="badge badge-outline">Rust</div>
+					</div>
+					<div>
+						<div class="badge badge-outline">Try</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
