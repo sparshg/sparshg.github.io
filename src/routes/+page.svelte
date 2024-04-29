ef942dae00d9a01e8c5480039be35e4c61c43740
sparshg <43041139+sparshg@users.noreply.github.com>
2024-04-30 03:34:20 +0530

    add contacts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,7 +11,7 @@
 
 <div class="lg:flex mx-12 my-16">
 	<div class="lg:w-[55%] max-lg:w-0" />
-	<div class="lg:fixed lg:w-[45%] lg:h-[85vh] flex flex-col justify-between">
+	<div class="lg:fixed lg:w-[45%] lg:h-[calc(100vh-7rem)] flex flex-col justify-between">
 		<div>
 			<h1 class="font-bold text-6xl mb-7">Sparsh Goenka</h1>
 			<Typewriter input={text} />
@@ -21,17 +21,43 @@
 				a.created_at && b.created_at ? a.created_at.getTime() - b.created_at.getTime() : 0
 			)}
 		/>
-		<div class="flex justify-between">
-			<div class="avatar">
-				<div class="w-16 rounded-full">
-					<img src="https://avatars.githubusercontent.com/u/43041139" alt="user-icon" />
+		<div class="flex justify-between items-center">
+			<div class="flex items-center gap-3">
+				<div class="avatar">
+					<div class="w-16 rounded-full">
+						<img src="https://avatars.githubusercontent.com/u/43041139" alt="user-icon" />
+					</div>
 				</div>
+				<a
+					href="https://github.com/sparshg"
+					target="_blank"
+					rel="noopener noreferrer"
+					class="badge badge-primary py-4 px-3 ml-8 transition hover:border-b-base-content"
+				>
+					<Icon icon="github" class="size-4 mr-2" />Github<Icon icon="link" />
+				</a>
+				<a
+					href="https://youtube.com/c/radiium"
+					target="_blank"
+					rel="noopener noreferrer"
+					class="badge badge-primary py-4 px-3 transition hover:border-b-base-content"
+				>
+					<Icon icon="youtube" class="size-4 mr-2" />Youtube<Icon icon="link" />
+				</a>
+				<a
+					href="https://linkedin.com/in/sparshgoenka"
+					target="_blank"
+					rel="noopener noreferrer"
+					class="badge badge-primary py-4 px-3 transition hover:border-b-base-content"
+				>
+					<Icon icon="linkedin" class="size-4 mr-2" />Linkedin<Icon icon="link" />
+				</a>
 			</div>
 
-			<label class="swap swap-rotate">
+			<label class="swap swap-rotate bg-base-300 rounded-full size-16">
 				<input type="checkbox" class="theme-controller" value="cupcake" />
-				<Icon icon="sun" />
-				<Icon icon="moon" />
+				<Icon icon="sun" class="swap-on size-8" />
+				<Icon icon="moon" class="swap-off size-8" />
 			</label>
 		</div>
 	</div>
